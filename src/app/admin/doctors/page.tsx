0bfb31d9a4fa419da568471f058ddc727bdcbc95
isotop786 <marufislam77@gmail.com>
2024-05-09 11:55:26 +0600

    page error fixed

--- src/app/admin/doctors/page.tsx
+++ src/app/admin/doctors/page.tsx
@@ -15,17 +15,25 @@ const DoctorsPage = () => {
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">New message</h1>
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Add New Doctor</h1>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-        <form>
+        <>
           <div className="mb-3">
-            <label htmlFor="recipient-name" className="col-form-label">First Name</label>
-            <input type="text" className="form-control" id="recipient-name"/>
+            <label htmlFor="firstName" className="col-form-label">First Name</label>
+            <input type="text" className="form-control" id="firstName"/>
           </div>
           <div className="mb-3">
-            <label htmlFor="recipient-name" className="col-form-label">Last Name</label>
+            <label htmlFor="lastName" className="col-form-label">Last Name</label>
+            <input type="text" className="form-control" id="lastName"/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="col-form-label">Email </label>
+            <input type="text" className="form-control" id="email"/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="recipient-name" className="col-form-label">Image URL</label>
             <input type="text" className="form-control" id="recipient-name"/>
           </div>
           <div className="mb-3">
@@ -38,7 +46,7 @@ const DoctorsPage = () => {
                 </select>        
             {/* <textarea className="form-control" id="message-text"></textarea> */}
           </div>
-        </form>
+        </>
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -63,20 +71,21 @@ const DoctorsPage = () => {
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Cardiac</td>
-      <td>Emergency</td>
+      <td>Howard</td>
+      <td>Hamlin</td>
       <td>Dental</td>
     </tr>
     <tr>
       <th scope="row">2</th>
       <td>Jacob</td>
       <td>Thornton</td>
-      <td>@fat</td>
+      <td>Emmergency</td>
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td >Larry the Bird</td>
-      <td>@twitter</td>
+      <td >Larry</td>
+      <td>Paige</td>
+      <td>Cardiarc</td>
     </tr>
   </tbody>
 </table>
